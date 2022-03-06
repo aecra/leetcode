2c896b5cbfc86936ea4ea1a6b3cc5832765d93d3
aecra <admin@aecra.cn>
2022-03-07 01:03:00 +0800

    finish problem 557

---
+++ 557.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func reverseWords(s string) string {
+	res := []byte{}
+	for i := 0; i < len(s); {
+		start := i
+		for i < len(s) && s[i] != ' ' {
+			i++
+		}
+
+		for point := start; point < i; point++ {
+			res = append(res, s[start+i-1-point])
+		}
+
+		for i < len(s) && s[i] == ' ' {
+			i++
+			res = append(res, ' ')
+		}
+	}
+	return string(res)
+}
