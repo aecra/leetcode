1f9179e6ac8cb000546dab20555b5ad874321e4c
aecra <admin@aecra.cn>
2022-08-22 23:11:13 +0800

    finish problem 309

--- leetcode_test.go
+++ leetcode_test.go
@@ -117,6 +117,22 @@ func TestBuildTree(t *testing.T) {
 	}
 }
 
+// 309. Best Time to Buy and Sell Stock with Cooldown
+// 309. 买卖股票的最佳时机含冷冻期
+func TestMaxProfit309(t *testing.T) {
+	// test case 1
+	got := maxProfit309([]int{1, 2, 3, 0, 2})
+	if got != 3 {
+		t.Errorf("maxProfit309() error %d.", got)
+	}
+
+	// test case 2
+	got = maxProfit309([]int{1})
+	if got != 0 {
+		t.Errorf("maxProfit309() error %d.", got)
+	}
+}
+
 // 343. Integer Break
 // 343. 整数拆分
 func TestIntegerBreak(t *testing.T) {
